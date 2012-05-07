9340d240a8da57b5078124819f4a6f66ff533560
Sameer_Arya <sarya@stanford.edu>
2012-05-07 16:25:54 +0900

    Added branch testing

--- lab2/irom.v
+++ lab2/irom.v
@@ -234,13 +234,20 @@ module irom(clka, addra, douta);
   assign memory[ 91] = {`NOP};
   assign memory[ 92] = {`NOP};
   assign memory[ 93] = {`NOP};
-  assign memory[ 94] = {`NOP};
-  assign memory[ 95] = {`NOP};
-  assign memory[ 96] = {`NOP};
-  assign memory[ 97] = {`NOP};
-  assign memory[ 98] = {`NOP};
-  assign memory[ 99] = {`NOP};
-  assign memory[100] = {`NOP};
+  //beq $t2, $t2, offset
+  assign memory[ 94] = {`BEQ, 'T2, 'T2, -16d'2};
+  //bne $t3, $t2, offset
+  assign memory[ 95] = {`BNE, 'T2, 'T3, -16d'2};
+  //bltz $t3, offset
+  assign memory[ 96] = {`BLTZ, 'NULL, 'T3, -16d'2};
+  //blez $t3, offset
+  assign memory[ 97] = {`BLEZ, 'NULL, 'T3, -16d'2};
+  //bgtz $t2, offset
+  assign memory[ 98] = {`BGTZ, `NULL, `T2, -16d'2};
+  //bgez $t2, offset
+  assign memory[ 99] = {`BGEZ, `NULL, `T2, -16d'2};
+  //lui $t0, 50
+  assign memory[100] = {`LUI, `NULL, `T0, 16d'50};
   assign memory[101] = {`NOP};
   assign memory[102] = {`NOP};
   assign memory[103] = {`NOP};
